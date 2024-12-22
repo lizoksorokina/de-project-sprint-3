224db5391167847934d4a682265bec450561ee53
Sorokina Elizaveta <sorokina_liza@icloud.com>
2024-12-22 23:50:55 +0300

    add data mart

---
+++ migrations/add_data_mart.sql
@@ -0,0 +1,13 @@
+drop table if exists mart.f_customer_retention
+create table if not exists mart.f_customer_retention (
+    id bigint,
+    new_customers_count bigint,
+    returning_customers_count bigint,
+    refunded_customer_count bigint,
+    period_name varchar(50),
+    period_id varchar(50),
+    item_id varchar(50),
+    new_customers_revenue float,
+    returning_customers_revenue float,
+    customers_refunded bigint
+);
